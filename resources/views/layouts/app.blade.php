4c42673c606a29c11d31f3d984828e04d65ee0f8
Md Shahidul Islam <contact.shahidul@gmail.com>
2023-01-07 11:53:27 +0600

    update app logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,10 +44,9 @@
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home') }}">
-                <div class="sidebar-brand-icon rotate-n-15">
+                {{-- <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">{{ auth()->user()->name }} Panel</div>
+                </div> --}}
                 <div class="sidebar-brand-text mx-3">{{ auth()->user()->name }} Panel</div>
             </a>
 
